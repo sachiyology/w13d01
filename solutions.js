1c6a6db9b53f9b261579d4490b26037bfc2227c7
sachiyo sugaya <sachiyology@gmail.com>
2021-07-21 08:26:41 +0900

    verbal questions completed

---
+++ solutions.js
@@ -0,0 +1,7 @@
+1)
+Function parameters are the names listed in the function's definition.
+Function arguments are the real values passed to the function.
+
+2)
+return returns a value to the caller of that function.
+console.log just logs out information in console.
