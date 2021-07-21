acefc35b16824da4b14c1594cd0011a61e1e0599
sachiyo sugaya <sachiyology@gmail.com>
2021-07-22 07:30:39 +0900

    sum array completed

--- solutions.js
+++ solutions.js
@@ -17,3 +17,19 @@ function checkPalindrome(str) {
 }
 console.log(checkPalindrome("Radar"));
 console.log(checkPalindrome("Borscht"));
+
+4)
+Write a function sumArraythat takes an array as an argument.
+The array should contain numbers.
+The function should return the sum of the numbers in the array.
+Use a for loop within the function to iterate over the array and sum the contents.
+Use a variable such as
+
+function sumArray(a){
+let sum = 0;
+for (let i = 0; i < a.length; i++) {
+sum += a[i];
+}
+return sum;
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
